b722a6432ee53f3a8a85a4628fe5a5c19a387eed
53845714nF <hackwiki.snort118@passinbox.com>
2026-03-26 17:34:27 +0100

    feat: Add a health check endpoint and integrate it into the Dockerfile.

--- src/cmd/api-server/main.go
+++ src/cmd/api-server/main.go
@@ -6,12 +6,25 @@ import (
 	"hasura-jwt/internal/config"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "healthcheck" {
+		res, err := http.Get("http://127.0.0.1:3000/health")
+		if err != nil || res.StatusCode != http.StatusOK {
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	config.LoadConfig()
 	fmt.Println("Starting Server ...")
 	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	mux.HandleFunc("/signup", auth.SignupHandler)
 	mux.HandleFunc("/login", auth.LoginHandler)
 	mux.HandleFunc("/verify/{token}", auth.VerifyHandler)
